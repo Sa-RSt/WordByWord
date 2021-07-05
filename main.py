b883a136ea1eccf40f9c482b4ca4f6cce6640e52
SamRSt <samu.rsteid@gmail.com>
2021-07-04 22:44:55 -0300

    Add icon

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
+
 from wordbyword.gui.app import App
 from wordbyword.gui.window import create_main_window
 
-wnd = create_main_window()
+wnd = create_main_window(os.path.join(os.path.dirname(__file__), 'icon.png'))
 app = App(wnd, wnd)
 wnd.rowconfigure(1, weight=1)
 wnd.columnconfigure(1, weight=1)
--- wordbyword/gui/window.py
+++ wordbyword/gui/window.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
-def create_main_window():
+def create_main_window(iconpath):
     wnd = tk.Tk()
+    wnd.iconphoto(True, tk.PhotoImage(file=iconpath))
     wnd.title('Word by Word Reader')
     return wnd
