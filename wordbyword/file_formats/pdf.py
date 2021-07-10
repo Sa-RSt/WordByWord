6876d90d5d0e6097754a0ff0a0416aa3c0b25186
SamRSt <samu.rsteid@gmail.com>
2021-07-09 21:56:07 -0300

    Collapse continuous whitespace chars

--- wordbyword/file_formats/pdf.py
+++ wordbyword/file_formats/pdf.py
@@ -1,5 +1,6 @@
 import pdfminer.high_level
 from pdfminer.pdftypes import PSException
+from string import whitespace
 
 from . import FileReader, check_extension
 
@@ -13,4 +14,8 @@ class PDFFileReader(FileReader):
         if not check_extension(filename, '.pdf'):
             return None
 
-        return pdfminer.high_level.extract_text(filename)
+        raw = pdfminer.high_level.extract_text(filename)
+        for char in whitespace:
+            while char + char in raw:
+                raw = raw.replace(char + char, char)
+        return raw
