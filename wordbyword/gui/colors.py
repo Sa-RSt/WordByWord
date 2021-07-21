bb39b1f5257f20cbce893946c26049ea4bd0adf4
SamRSt <samu.rsteid@gmail.com>
2021-07-21 13:22:35 -0300

    Better dark theme colors

--- wordbyword/gui/colors.py
+++ wordbyword/gui/colors.py
@@ -1,6 +1,6 @@
 # Colors here are represented in the form (Light mode, Dark mode)
 
-BACKGROUND = ('#f0f0f0', '#373737')
-DISPLAY = ('#ffffff', '#000000')
+BACKGROUND = ('#f0f0f0', '#182541')
+DISPLAY = ('#ffffff', '#231942')
 TEXT = ('#000000', '#ffffff')
-BUTTON = ('#d3d3d3', '#3a3a3a')
+BUTTON = ('#d3d3d3', '#123a3a')
