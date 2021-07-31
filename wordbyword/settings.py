600f494cd9f0ebf5e59116f9bcd15afe6d532bce
SamRSt <samu.rsteid@gmail.com>
2021-07-31 19:49:05 -0300

    Fix settings

--- wordbyword/settings.py
+++ wordbyword/settings.py
@@ -2,8 +2,21 @@ import json
 import os
 
 SETTINGS_FILE = os.path.expanduser('~/.wbwr_cfg')
+DEFAULT_SETTINGS = {
+    'blink_warning_shown': False,
+    'night_mode': True,
+    'speed': 300
+}
 
 class _settings_type(dict):
+    def __getitem__(self, name):
+        try:
+            return super(_settings_type, self).__getitem__(name)
+        except KeyError:
+            val = DEFAULT_SETTINGS[name]
+            self[name] = val
+            return val
+
     def save(self):
         with open(SETTINGS_FILE, 'w') as file:
             json.dump(self, file)
@@ -18,7 +31,4 @@ Settings = _settings_type()
 try:
     Settings.load()
 except OSError:
-    Settings['version'] = 1
-    Settings['night_mode'] = True
-    Settings['blink_warning_shown'] = False
-    Settings.save()
+    pass  # Settings will be populated and saved on demand
